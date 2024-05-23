5264eb663a9c454fea6ed011805bb1a0f9021d51
Sagarika Nayak <sagarikanayak544@gmail.com>
2024-05-24 00:29:41 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="sagarikanayak544@gmail.com">sagarikanayak544@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/11-sagarika-nayak/">linkedin.com/11-sagarika-nayak</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Sagarika1119">github.com/Sagarika1119</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
